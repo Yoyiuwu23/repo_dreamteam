cc1a252977ece4a0a193c4215ba81c44284b0c8b
FrancoCidSanMartin <francocid99gmail.com>
2025-10-08 21:01:01 -0300

    Redireccion a home

--- routers/usuarios.py
+++ routers/usuarios.py
@@ -1,6 +1,6 @@
 # routers/usuarios.py
 from fastapi import APIRouter, HTTPException, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from dto.usuario import UsuarioLogin, UsuarioResponse
 from models.usuarios import UsuariosModel
@@ -19,7 +19,7 @@ def login_post(request: Request, email: str = Form(...), password: str = Form(..
     if not usuario:
         return templates.TemplateResponse("login.html", {"request": request, "titulo": "Login | Finantel Group", "error": "Credenciales inválidas"})
     # Pasa al home si login correcto
-    return templates.TemplateResponse("home.html", {"request": request, "titulo": "Home | Finantel Group", "user": usuario})
+    return RedirectResponse(url="/home", status_code=303)
 
 # Ruta API para login por JSON (POST /usuarios/loginjson)
 @router.post("/loginjson", response_model=UsuarioResponse)
